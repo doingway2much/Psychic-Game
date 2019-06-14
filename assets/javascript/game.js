63b9c747ea466b0580752b2d6fc8d8f1b66a6986
Seth Randell <sethhrandell@gmail.com>
2019-06-14 17:48:00 -0400

    fixed game so it picks a new letter after a win and or loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,43 +3,45 @@ var numberOfWins = 0;
 var losses = 0;
 var guesses = 0;
 var userGuessed = [];
-var computerGussed = [];
+var computerGuessed = [];
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var numGuess = "10"
 
-
-// Generate random letter for computer guess
-
-var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-console.log("The Psychic picked: " + getRandomLetter);
-
+    // Function that generates random letter for computer guess
+    window.onload = function() {
+        var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+        computerGuessed.push(getRandomLetter);
+        console.log("The Psychic picked: " + computerGuessed[0]);
+        }
 
 // Create user variable for user guess on key press
 document.onkeyup = function(event) {
     var userGuess = event.key
     userGuessed.push(userGuess);
+    
+    //Pass 
     var guess = document.getElementById("history");
     guess.textContent = "Previous guesses: " + userGuessed;
     console.log(userGuessed);
-    console.log("The user userGuessed " + userGuess);
+    console.log("The user guessed " + userGuess);
 
     // If the users guesses the right anwser
 
-
-    if(userGuess === getRandomLetter) {
-        console.log("Looks like you userGuessed the righ anwser");
+    if(userGuess === computerGuessed[0]) {
+        console.log("Looks like you guessed the righ anwser");
         numberOfWins++;
         var wins = document.getElementById("wins");
         wins.textContent = "Number of Wins: " + numberOfWins;
         console.log(numberOfWins);
         numGuess = 10;
         userGuessed = [];
-        // var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-        // console.log("The Psychic picked a new letter: " + getRandomLetter);
-        // If the user gets the wrongn anwser
+        computerGuessed.length = 0;
+        var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+        computerGuessed.push(getRandomLetter);
+        console.log("The Psychic picked another letter: " + computerGuessed[0]);
         }else {
-            if(userGuess !== getRandomLetter) {
-                console.log("Looks like you userGuessed the wrong anwser");
+            if(userGuess !== computerGuessed[0]) {
+                console.log("Looks like you guessed the wrong anwser");
                 numGuess--;
                 var wrong = document.getElementById("remaining");
                 wrong.textContent = "Guesses remaning: " + numGuess;
@@ -52,6 +54,10 @@ document.onkeyup = function(event) {
                 userGuessed = [];
                 var gameOver = document.getElementById("greeting");
                 gameOver.textContent = "Guess what letter I'm thinking of......Sorry game over!!!!!";
+                computerGuessed.length = 0;
+                var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+                computerGuessed.push(getRandomLetter);
+                console.log("The Psychic picked another letter: " + computerGuessed[0]);
                 }
 
         }
