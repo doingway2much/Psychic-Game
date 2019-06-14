1358609d636cf934d0f4e41b535f6fa19f7a9d0e
Seth Randell <sethhrandell@gmail.com>
2019-06-14 12:53:06 -0400

    added computer guess to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,4 +3,14 @@ var wins = 0;
 var losses = 0;
 var guesses = 0;
 var guesed ="";
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
 
+
+var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+console.log("The Psychic picked: " + getRandomLetter);
+
+
+document.onkeyup = function(event) {
+    var userGuess = event.key
+    console.log("The user guessed " + userGuess)
+}
